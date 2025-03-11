bd22c5f54a1f50e46eb5f91e5a9bd1f2c5292fd2
anass17 <anassboutaib2018@gmail.com>
2025-03-11 12:04:39 +0000

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,24 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CountryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return response()->json(['user' => $request->user()], 200);
-// })
-// ->middleware('auth:sanctum')
-// ;
+// User Routes
 
+Route::get('/user', function (Request $request) {
+    return response()->json(['user' => $request->user()], 200);
+})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/store', [AuthController::class, 'store']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
+
+// Country Routes
+
+Route::resource('countries', CountryController::class);
